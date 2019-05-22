4cbb0ea1c2c25477ed67212bd20afc8c210c071d
garza <danni900g@gmail.com>
2019-05-21 22:36:01 -0600

    commit inicial

---
+++ cmake-build-debug/CMakeFiles/MyDataBase.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyDataBase.dir/main.cpp.obj"
+  "MyDataBase.pdb"
+  "MyDataBase.exe"
+  "MyDataBase.exe.manifest"
+  "libMyDataBase.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyDataBase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
